b337b8720c831af5aeeed48376736bf952f45d98
Piyush Mishra <ifs.piyushmishra1991@gmail.com>
2022-08-16 21:45:46 +0530

    CodingNinjas-AlarmClock- Current Time Display Added

--- Contents/CodingNinjas/FrontendOnly/AlarmClock/scripts/currentTime.js
+++ Contents/CodingNinjas/FrontendOnly/AlarmClock/scripts/currentTime.js
@@ -1,1 +1,20 @@
-console.log(document.getElementById("current-time-text"));
+// Caching DOM reference:
+const currentTimeDisplay = document.getElementById("current-time-text");
+
+function setCurrentTime() {
+  const currentTime = new Date();
+  const formattedTime = timeFormatter(currentTime);
+  currentTimeDisplay.innerText = formattedTime;
+}
+
+function timeFormatter(timeGiven) {
+  return (
+    String(timeGiven.getHours()).padStart(2, "0") +
+    ":" +
+    String(timeGiven.getMinutes()).padStart(2, "0") +
+    ":" +
+    String(timeGiven.getSeconds()).padStart(2, "0")
+  );
+}
+
+setInterval(setCurrentTime, 1000);
